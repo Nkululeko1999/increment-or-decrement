148669ffd151b03d9ca95a7d2f5eb5b5c84523ff
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2023-09-24 12:09:13 +0200

    Modified css file and added raect hooks on count var in app.jsx

--- increment/src/App.js
+++ increment/src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  function incrementValue(){
+    setCount(count + 1)
+  }
+
+  function decrementValue(){
+    setCount(count - 1)
+  }
   return (
     <div className="App">
       <div className="container">
-        <h1>0</h1>
+        <h1>{count}</h1>
         <p className="message">hh</p>
-        <button className="decrement-btn">-</button>
-        <button className="increment-btn">+</button>
+        <button onClick={decrementValue} className="decrement-btn">-</button>
+        <button onClick={incrementValue} className="increment-btn">+</button>
       </div>
     </div>
   );
