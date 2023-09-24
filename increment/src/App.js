3bad96a98c36479f6eacea7cda5ef6a8a57eec2c
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2023-09-24 12:02:10 +0200

    created static files, modified index.css, html file na app.jsx

--- increment/src/App.js
+++ increment/src/App.js
@@ -1,6 +1,12 @@
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <h1>0</h1>
+        <p className="message">hh</p>
+        <button className="decrement-btn">-</button>
+        <button className="increment-btn">+</button>
+      </div>
     </div>
   );
 }
