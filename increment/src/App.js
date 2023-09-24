791edd2a6b085676d09ef391d0cb9d27d7c1ef23
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2023-09-24 12:16:23 +0200

    function to handle message when count is out of range

--- increment/src/App.js
+++ increment/src/App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // const [message, setMessage] = useState("");
 
   function incrementValue(){
     setCount(count + 1)
@@ -11,11 +12,17 @@ function App() {
   function decrementValue(){
     setCount(count - 1)
   }
+
+  // function handleMessage(){
+
+  // }
+
+  console.log(count);
   return (
     <div className="App">
       <div className="container">
         <h1>{count}</h1>
-        <p className="message">hh</p>
+        <p className="message"></p>
         <button onClick={decrementValue} className="decrement-btn">-</button>
         <button onClick={incrementValue} className="increment-btn">+</button>
       </div>
